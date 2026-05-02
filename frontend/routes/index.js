d63bbf5075c575b7c41987c7073013df562ac8ef
Vincent Dao <vincent.dao@yahoo.com>
2026-05-02 05:23:27 -0500

    commit

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,21 +1,88 @@
-var express =   require("express");
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
-// load index page 
-router.get('/',function(req, res){
-  res.render('index');
+const API = "http://localhost:5000";
+
+// MEMBERS
+
+// CREATE
+router.post('/members/add', async (req, res) => {
+    await axios.post(`${API}/members`, req.body);
+    res.redirect('/');
 });
 
-//recive message from form
-router.post('/send-message',function(req, res){
-  var obj = {
-    name: req.body.name,
-    email:req.body.email,
-    phone_number: req.body.phone_number,
-    message: req.body.message
+// READ
+// router.get('/', async (req, res) => {
+//    const members = await axios.get(`${API}/members`);
+//    res.render('index', { members: members.data });
+//});
+
+// UPDATE
+router.post('/members/update/:id', async (req, res) => {
+    await axios.put(`${API}/members/${req.params.id}`, req.body);
+    res.redirect('/');
+});
+
+// DELETE
+router.post('/members/delete/:id', async (req, res) => {
+    await axios.delete(`${API}/members/${req.params.id}`);
+    res.redirect('/');
+});
+
+// EVENTS
+
+// CREATE
+router.post('/events/add', async (req, res) => {
+    await axios.post(`${API}/events`, req.body);
+    res.redirect('/');
+});
+
+// READ
+router.get('/', async (req, res) => {
+  try {
+    const [membersRes, eventsRes, registrationsRes] = await Promise.all([
+        axios.get(`${API}/members`),
+        axios.get(`${API}/events`),
+        axios.get(`${API}/registrations`)
+    ]);
+
+    res.render('index', {
+        members: membersRes.data,
+        events: eventsRes.data,
+        registrations: registrationsRes.data
+    });
+  } catch (err) {
+    console.error("Error fetching data:", err.message);
+    // Provide defaults so the page doesn't crash if one API call fails
+    res.render('index', { members: [], events: [], registrations: [] });
   }
-  //return message with parms form
-  res.send("Recived message: "+obj.name +" "+ obj.email +" "+ obj.phone_number +" "+ obj.message);
+});
+
+// UPDATE
+router.post('/events/update/:id', async (req, res) => {
+    await axios.put(`${API}/events/${req.params.id}`, req.body);
+    res.redirect('/');
+});
+
+// DELETE
+router.post('/events/delete/:id', async (req, res) => {
+    await axios.delete(`${API}/events/${req.params.id}`);
+    res.redirect('/');
+});
+
+// REGISTRATIONS
+
+// CREATE
+router.post('/registrations/add', async (req, res) => {
+    await axios.post(`${API}/registrations`, req.body);
+    res.redirect('/');
+});
+
+// DELETE
+router.post('/registrations/delete/:id', async (req, res) => {
+    await axios.delete(`${API}/registrations/${req.params.id}`);
+    res.redirect('/');
 });
 
 module.exports = router;
